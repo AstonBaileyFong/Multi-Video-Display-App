6c0aa802bd04a2f95fa5d9576ce786513d67131b
aston <aston.bailey.fong@gmail.com>
2025-02-06 14:08:48 +1100

    New menu system.

--- app/src/main/java/com/example/multivideodisplayapp/MainActivity.kt
+++ app/src/main/java/com/example/multivideodisplayapp/MainActivity.kt
@@ -11,28 +11,74 @@ import android.widget.MediaController
 import android.widget.Toast
 import android.widget.VideoView
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.multivideodisplayapp.ImageViewModel
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.system.exitProcess
 
 
 class MainActivity :  AppCompatActivity() {
 
 
-
+    // Initialise the DrawerLayout, NavigationView and ToggleBar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
     lateinit var mediaControl: MediaController
     lateinit var mediaControl2: MediaController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Call findViewById on the DrawerLayout
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        // Pass the ActionBarToggle action into the drawerListener
+        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout.addDrawerListener(actionBarToggle)
+
+        // Display the hamburger icon to launch the drawer
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Call syncState() on the action bar so it'll automatically change to the back button when the drawer layout is open
+        actionBarToggle.syncState()
+
+
+        // Call findViewById on the NavigationView
+        navView = findViewById(R.id.navView)
+
+        // Call setNavigationItemSelectedListener on the NavigationView to detect when items are clicked
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.about -> {
+                    Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.exit -> {
+                    Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show()
+                    true
+                    exitProcess(0)
+                }
+                else -> {
+                    false
+                }
+            }
+        }
 //        var ImageOne: ImageView = findViewById(R.id.ImageOne)
 //        var ImageTwo: ImageView = findViewById(R.id.ImageTwo)
 //        ImageViewModel().InitialiseImage("ImageOne", "Image", ImageOne)
@@ -84,20 +130,22 @@ class MainActivity :  AppCompatActivity() {
             false
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
-        return super.onCreateOptionsMenu(menu)
+    // override the onSupportNavigateUp() function to launch the Drawer when the hamburger icon is clicked
+    override fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(navView)
+        return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
-            R.id.about -> Toast.makeText(this,"About Selected",Toast.LENGTH_SHORT).show()
-            R.id.settings -> Toast.makeText(this,"Settings Selected",Toast.LENGTH_SHORT).show()
-            R.id.exit -> Toast.makeText(this,"Exit Selected",Toast.LENGTH_SHORT).show()
+    // override the onBackPressed() function to close the Drawer when the back button is clicked
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
-        return super.onOptionsItemSelected(item)
     }
+
+
 
 
 }
